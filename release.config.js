7afa8cafb43953cafe4428f72d8121aecf7580d2
victory-aime <130195331+victory-aime@users.noreply.github.com>
2026-05-19 15:10:21 +0100

    refactor(config): update release.config.js to remove unused plugins
    Removed changelog and git release configuration.

--- release.config.js
+++ release.config.js
@@ -20,15 +20,7 @@ module.exports = {
       },
     ],
     '@semantic-release/release-notes-generator',
-    '@semantic-release/changelog',
     ['@semantic-release/npm', { npmPublish: false }],
-    [
-      '@semantic-release/git',
-      {
-        assets: ['CHANGELOG.md', 'package.json', 'pnpm-lock.yaml'],
-        message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
-      },
-    ],
     '@semantic-release/github',
   ],
   tagFormat: 'v${version}',
