050e362bcc7714e14b64c15953099e0dbb1ed3b3
victory-aime <130195331+victory-aime@users.noreply.github.com>
2026-05-19 15:04:10 +0100

    refactor(config): configure commit analyzer with release rules
    Update commit analyzer configuration to use conventional commits with specific release rules.

--- release.config.js
+++ release.config.js
@@ -1,7 +1,24 @@
 module.exports = {
   branches: ['main', { name: 'uat', prerelease: 'uat' }, { name: 'develop', prerelease: 'dev' }],
   plugins: [
-    '@semantic-release/commit-analyzer',
+    [
+      '@semantic-release/commit-analyzer',
+      {
+        preset: 'conventionalcommits',
+        releaseRules: [
+          { type: 'refactor', release: 'patch' },
+          { type: 'perf', release: 'patch' },
+          { type: 'fix', release: 'patch' },
+          { type: 'feat', release: 'minor' },
+          { type: 'docs', release: false },
+          { type: 'chore', release: false },
+          { type: 'test', release: false },
+          { type: 'style', release: false },
+          { type: 'ci', release: false },
+          { type: 'build', release: false },
+        ],
+      },
+    ],
     '@semantic-release/release-notes-generator',
     '@semantic-release/changelog',
     ['@semantic-release/npm', { npmPublish: false }],
